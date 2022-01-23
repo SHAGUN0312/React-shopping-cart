e07b5716dcc5334a4f31f4ffb2405ec8652ff630
Shagun Bagrecha <54242232+SHAGUN0312@users.noreply.github.com>
2022-01-23 09:39:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: "1",
+        name: "Macbook",
+        price: 1400,
+        image: "https://picsum.photos/id/180/2400/1600"
+      },
+      {
+        id: "2",
+        name: "Dream Catcher",
+        price: 200,
+        image: "https://picsum.photos/id/104/2400/1600"
+      },
+      {
+        id: "3",
+        name: "Maize(LOL)",
+        price: 600,
+        image: "https://picsum.photos/id/112/2400/1600"
+      }
+    ]
+  };
+
+  export default data;
